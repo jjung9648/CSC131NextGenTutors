495690e7c0a479d96457046b087d79197894b73b
Tuan Ton <tonthattuanst@gmail.com>
2024-11-12 13:23:11 -0800

    update index.php and landing-page.html

--- index.php
+++ index.php
@@ -7,11 +7,15 @@ require_once './Backend/includes/functions.php';
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 
 switch ($page) {
+    case 'landing-page':
+        include './Frontend/landing-page.html';
+        break;
+    case 'login':
+        include './Frontend/login.html';
+        break;
+
     case 'home':
         include './Backend/includes/home.php';
-        break;
-    case 'about':
-        include './Backend/includes/about.php';
         break;
     default:
         include './Backend/includes/404.php';
