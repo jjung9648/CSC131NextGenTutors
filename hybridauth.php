ab4a02a5ff7fe56f7d95bdd95d98ede52f5f0897
Tuan Ton <tonthattuanst@gmail.com>
2024-11-12 22:36:13 -0800

    Done with log in through Google API

--- hybridauth.php
+++ hybridauth.php
@@ -1,11 +1,16 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 return [
     'callback' => 'http://localhost:8080/?page=callback',
     'providers' => [
         'Google' => [
             'enabled' => true,
             'keys' => [
-               'id' => $_ENV['GOOGLE_CLIENT_ID'],
+                'id' => $_ENV['GOOGLE_CLIENT_ID'],
                 'secret' => $_ENV['GOOGLE_CLIENT_SECRET']
             ],
         ],
